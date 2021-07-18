bc67edf12eab236cf2cf6ebafeb8364137401759
vincent <osf@hotmail.fr>
2021-07-18 17:38:27 +0200

    Set up Cypress E2E tests

--- cypress/integration/smoke.spec.js
+++ cypress/integration/smoke.spec.js
@@ -1,5 +1,6 @@
-describe("Page Should contain learn react", () => {
-  it("Should attempt Home page", () => {
-    cy.visit("http://localhost:3000").contains("Learn React");
+describe('Page Should contain learn react', () => {
+  it('Should attempt Home page', () => {
+    cy.visit('/');
+    cy.contains('Learn React');
   });
 });
